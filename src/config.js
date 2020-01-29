84959de619c58dc97ea1e51e1794b0fb39c70642
Tony Martinez <ajosephmartinez@gmail.com>
2020-01-28 19:27:12 -0800

    .

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
   s3: {
-    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-east-1",
+    BUCKET: "notes-app-bucket-tony-m"
   },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "us-east-1",
+    URL: "https://ivtb1b5pnf.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    REGION: "ca-central-1",
+    USER_POOL_ID: "ca-central-1_py7b6V72w",
+    APP_CLIENT_ID: "4b2d3mukq02l7e4g2p1qc5jmor",
+    IDENTITY_POOL_ID: "ca-central-1:27d81374-030d-431b-918d-8dd7f11f87af"
   }
 };
