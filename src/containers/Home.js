dfe943962069fb16d43feb13dfdce0fc11623bd1
Tony Martinez <ajosephmartinez@gmail.com>
2020-02-12 22:39:13 -0800

    .

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import { API } from "aws-amplify";
 
@@ -55,6 +56,14 @@ export default function Home(props) {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
